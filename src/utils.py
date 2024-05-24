1748130b3a545d2da457678f5faaea2e1e499023
Boris Smus <boris@smus.com>
2024-05-23 23:14:29 -0700

    social media generator and start of refactor

---
+++ src/utils.py
@@ -0,0 +1,44 @@
+from pydantic import BaseModel, ConfigDict
+import csv
+
+class Invention(BaseModel):
+  model_config = ConfigDict(strict=True)
+  title: str
+  year: str
+  summary: str
+  inventor: str
+  location: str
+  field: str
+  related: str
+  description: str = None
+  id: str = None
+
+  def year_number(self):
+    if (self.year.endswith('BCE')):
+      return -int(self.year[:-3])
+
+    return int(self.year)
+
+def load_inventions(tsv_path) -> list[Invention]:
+  with open(tsv_path, "r") as file:
+    reader = csv.reader(file, delimiter="\t", quotechar='"')
+    inventions = [Invention(**invention_from_tsv(row)) for row in reader]
+    return inventions
+
+def invention_from_tsv(fields: list[str]) -> dict:
+  if len(fields) <= 8:
+    # print(fields)
+    print(f"Warning: Invalid TSV format. Only {len(fields)} fields found.")
+    if len(fields) == 7:
+      # Unknown field.
+      fields.append("Unknown")
+  return {
+    "id": fields[0],
+    "year": fields[1],
+    "title": fields[2],
+    "summary": fields[3],
+    "inventor": fields[4],
+    "location": fields[5],
+    "related": fields[6],
+    "field": fields[7],
+  }
